161c5c6d5e0363315c324e7398742a6a49795573
Alex Kukushkin <alex@kukushk.in>
2018-05-23 15:31:12 +0200

    Added mock/request

--- lib/mimi/messaging/mock.rb
+++ lib/mimi/messaging/mock.rb
@@ -7,6 +7,7 @@
 
 require_relative 'mock/connection'
 require_relative 'mock/request_processor'
+require_relative 'mock/request'
 
 Mimi::Messaging.send :remove_const, :Connection
 Mimi::Messaging::Connection = Mimi::Messaging::MockConnection
